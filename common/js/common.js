094751c9ba0352b304eb1392fd5fbc87628cb67e
DESKTOP-E8IMCJ4\akonitto <hamakazelove@gmail.com>
2021-05-18 14:23:18 +0900

    画像ファイル名変更、画像の変更機能、シャッター実装途中

--- common/js/common.js
+++ common/js/common.js
@@ -1,2 +1,55 @@
-var layer = $("#layer");
-var parallax = new Parallax(layer);
+// 背景変更
+function changePage(e){
+    switch(e){
+        case "top": // top背景
+            $('body').css('background-image', 'url(../DemandLink-gitpages/common/img/back-ground-top.jpg)');
+            setValue(e);
+        break;
+
+        case "appli": // アプリ一覧背景
+            $('body').css('background-image', 'url(../DemandLink-gitpages/common/img/back-ground-appli.jpg)');
+            setValue(e);
+        break;
+
+        case "Inquiry": // 問い合わせ背景
+            $('body').css('background-image', 'url(../DemandLink-gitpages/common/img/back-ground-inquiry.jpg)');
+            setValue(e);
+        break;
+
+        case "help": // ヘルプ背景
+            $('body').css('background-image', 'url(../DemandLink-gitpages/common/img/back-ground-help.jpg)');
+            setValue(e);
+        break;
+    }
+}
+
+function setValue(e){
+    console.log("setvalue");
+    console.log(e);
+
+}
+
+
+function fade() {
+    var target = document.getElementById("fadeLayer");
+    target.style.visibility = "visible";
+  }
+
+
+
+function changeBoxColor( newColor ) {
+    document.getElementById('sakurabox').style.backgroundColor = newColor;
+}
+
+
+const CLASSNAME = "-visible";
+const TIMEOUT = 2000;
+const $target = $(".bg");
+
+setInterval(() => {
+  $target.addClass(CLASSNAME);
+
+  setTimeout(() => {
+    $target.removeClass(CLASSNAME);
+  }, TIMEOUT);
+}, TIMEOUT*2);
