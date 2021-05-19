71691c5fecf103eedacde6ee29cb06482bbdeb0a
DESKTOP-E8IMCJ4\akonitto <hamakazelove@gmail.com>
2021-05-19 16:54:37 +0900

    画面切り替えの実装 メール画面作成途中

--- common/js/common.js
+++ common/js/common.js
@@ -1,47 +1,51 @@
 // 背景変更
 function changePage(pageName){
-    var stats =  $("#page-stats");
-    console.log(stats);
+    var stats =  $("#page-stats").val();
 
-    switch(pageName){
-        case "a":
-            console.log("temp");
+    if (pageName !== stats) {
 
-        case "top": // top背景
-            setValue(pageName);
-            shutterFade(pageName);
-        break;
-
-        case "appli": // アプリ一覧背景
-            setValue(pageName);
-            shutterFade(pageName);
-        break;
-
-        case "Inquiry": // 問い合わせ背景
-            setValue(pageName);
-            shutterFade(pageName);
-        break;
-
-        case "help": // ヘルプ背景
-            setValue(pageName);
-            shutterFade(pageName);
-        break;
-
-        default: // それ以外の処理
-            return false;
+        switch(pageName){
+            case "top": // top背景
+                shutterFade(pageName);
+                changePageContent(stats, pageName);
+                setValue(pageName);
+            break;
+    
+            case "appli": // アプリ一覧背景
+                shutterFade(pageName);
+                changePageContent(stats, pageName);
+                setValue(pageName);
+            break;
+    
+            case "Inquiry": // 問い合わせ背景
+                shutterFade(pageName);
+                changePageContent(stats, pageName);
+                setValue(pageName);
+            break;
+    
+            case "help": // ヘルプ背景
+                shutterFade(pageName);
+                changePageContent(stats, pageName);
+                setValue(pageName);
+            break;
+    
+            default: // それ以外の処理
+                return false;
+        }
     }
 }
 
+// pageステータスの値変更
 function setValue(e){
-    console.log("setvalue");
+    $("#page-stats").val(e);
 }
 
-// page切り替え
+// 背景切り替えシャッター切り替え
 function shutterFade(pageName) {
     $('#shutter').removeClass('opne-shutter');
     $('#shutter').addClass('close-shutter');
     setTimeout(function(){
-        $('body').css('background-image', 'url(../DemandLink-GithubPage/common/img/back-ground-'+ pageName + '.jpg)');
+        $('body').css('background-image', 'url(../DemandLink-GithubPages/common/img/back-ground-'+ pageName + '.jpg)');
         $('#darkening').addClass('darkening');
     },2000);
 
@@ -53,8 +57,11 @@ function shutterFade(pageName) {
 }
 
 // pageコンテンツの表示変更
-function changePageContent(pageName){
-    console.log("changePage");
+function changePageContent(stats, pageName){
+    setTimeout(function(){
+        $("#main-content").children("." + stats).css("display","none");
+        $("#main-content").children("." + pageName).removeClass("none")
+    },2000);
 }
 
 
